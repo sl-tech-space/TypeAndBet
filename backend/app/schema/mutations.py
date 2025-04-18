8e86783e04e2fdf9308078db47cc478fee938877
ryo.matsumura <matsumuraryo0827@mail.com>
2025-04-18 21:35:13 +0900

    Google認証機能実装

--- backend/app/schema/mutations.py
+++ backend/app/schema/mutations.py
@@ -1,7 +1,9 @@
 import graphene
 from app.views.auth.register import RegisterUser
 from app.views.auth.login import LoginUser
+from app.views.auth.googleauth import GoogleAuth
 
 class Mutation(graphene.ObjectType):
     register_user = RegisterUser.Field()
-    login_user = LoginUser.Field() 
+    login_user = LoginUser.Field()
+    google_auth = GoogleAuth.Field() 
